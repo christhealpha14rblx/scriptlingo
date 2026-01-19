063b0f6deb10602862ce176097162a600e193f4e
christhealpha14rblx <christhealpha14rblx@gmail.com>
2026-01-18 17:49:54 -0700

    fixed a buncha stuff

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://xtlomubntxkkqgyjgvjj.supabase.co",
-  "sb_publishable_38Prs0hoaz6MImuH2IDs4w_nDIP-Wkl"
-);
